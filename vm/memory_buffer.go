21050054e847f1559ce8eaff5ec32510203fc5be
Eleanor McHugh <eleanor@games-with-brains.com>
2010-02-04 20:39:30 +0000

    Buffer now correctly accesses 'parent' class :)

--- vm/memory_buffer.go
+++ vm/memory_buffer.go
@@ -1,4 +1,5 @@
 //	TODO:	Storing and retrieving of pointers
+//	TODO:	Stream support?
 
 package vm
 
@@ -9,13 +10,13 @@ type Buffer struct {
 	IntVector
 }
 
-func (b *Buffer) Init(length int)						{ b.Resize(length, length) }
-func (b *Buffer) Len() int								{ return b.Len() }
-func (b *Buffer) Cap() int								{ return b.Cap() }
+func (b *Buffer) Init(length int)						{ b.IntVector.Resize(length, length) }
+func (b *Buffer) Len() int								{ return b.IntVector.Len() }
+func (b *Buffer) Cap() int								{ return b.Len() }
 func (b *Buffer) Slice(i, j int) *Buffer				{ return &Buffer{*b.IntVector.Slice(0, b.Len() - 1)} }
 func (b *Buffer) Clone() *Buffer						{ return b.Slice(0, b.Len() - 1) }
 func (b *Buffer) At(i int) int							{ return b.IntVector.At(i) }
-func (b *Buffer) Set(i, x int)							{ b.Set(i, x) }
+func (b *Buffer) Set(i, x int)							{ b.IntVector.Set(i, x) }
 func (b *Buffer) Add(i, x int)							{ b.Set(i, b.At(i) + x) }
 func (b *Buffer) Subtract(i, x int)						{ b.Set(i, b.At(i) - x) }
 func (b *Buffer) Multiply(i, x int)						{ b.Set(i, b.At(i) * x) }
@@ -31,3 +32,13 @@ func (b *Buffer) GreaterThan(i, x int) bool				{ return b.At(i) > x }
 func (b *Buffer) Copy(i, j int)							{ b.Set(i, b.At(j)) }
 func (b *Buffer) GetBuffer(i int) *Buffer				{ return (*Buffer)(unsafe.Pointer(uintptr(b.At(i)))) }
 func (b *Buffer) PutBuffer(i int, p *Buffer)			{ b.Set(i, int(uintptr(unsafe.Pointer(p)))) }
+
+//type Stream struct {
+//	Buffer
+//	index		int
+//}
+
+//func (b *Stream) Read(p []byte) (n int, err os.Error)	{ return 0, os.EOF }
+//func (b *Stream) Write(p []byte) (n int, err os.Error)	{ return 0, os.EOF }
+//func (b *Stream) Close() os.Error						{ return os.EOF }
+//func (b *Stream) Seek(offset int64, whence int) (ret int64, err os.Error)	{ return 0, os.EOF }
--- vm/memory_buffer_test.go
+++ vm/memory_buffer_test.go
@@ -1,20 +1,34 @@
 //	TODO: 	Write tests :)
 
 package vm
-import (
-	"testing"
-)
+import "testing"
+import "os"
 
 func populate(b *Buffer) {
 	b.Set(0, 37)
 	b.Set(1, int(byte("hello world"[1])))
-	b.Set(2, int(3.7))
+	f := 3.7
+	b.Set(2, int(f))
 }
 
-func Test_CreateBuffer(t *testing.T) {
+func TestCreateBuffer(t *testing.T) {
+	os.Stdout.WriteString("Testing Buffer Creation\n")
 	b := new(Buffer)
 	b.Init(6)
-	b.Len()
-	b.Cap()
 	populate(b)
+	if b.Len() != 6 {
+		t.Errorf("%T: B1) expected 6, got %d", b, b.Len())
+	}
+	if b.Cap() != 6 {
+		t.Errorf("%T: B2) expected 6, got %d", b, b.Cap())
+	}
+	if b.At(0) != 37 {
+		t.Errorf("%T: B3) expected 37, got %d", b, b.At(0))
+	}
+	if b.At(1) != int(byte("e"[0])) {
+		t.Errorf("%T: B4) expected 'e', got %d", b, b.At(1))
+	}
+	if b.At(2) != 3 {
+		t.Errorf("%T: B5) expected 3, got %d", b, b.At(2))
+	}
 }
