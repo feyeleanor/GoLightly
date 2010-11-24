444607eb5e6272cbea7a066ea4928d101bff6edc
Eleanor McHugh <eleanor@games-with-brains.com>
2010-11-24 17:22:11 +0000

    Updated TestTable to use slices internally, bypassing an error with trying to use non-hashable types as the axes values in the previous design.

--- test/test_table.go
+++ test/test_table.go
@@ -2,54 +2,45 @@ package testLightly
 
 import "fmt"
 
-type Reduction				func(y, x interface{}) interface{}
-
-type ResultSet map[interface{}] interface{}
+type Reduction		func(y, x interface{}) interface{}
+type ValueSet		[]interface{}
 
 type TestTable struct {
-	values			[]interface{}
-	compatibility	map[interface{}] ResultSet
+	XValues			ValueSet
+	YValues			ValueSet
+	compatibility	[]ValueSet
 	test			Reduction
 }
 
-func NewTestTable(test Reduction, values... interface{}) *TestTable {
+func NewTestTable(test Reduction) *TestTable {
 	t := new(TestTable)
 	t.test = test
-	t.X(values...)
 	return t
 }
 
 func (t *TestTable) X(values... interface{}) *TestTable {
-	t.values = values
-	t.compatibility = make(map[interface{}] ResultSet)
-	for _, v := range values {
-		t.compatibility[v] = make(ResultSet)
-	}
+	t.XValues = append(t.XValues, values...)
 	return t
 }
+
 func (t *TestTable) Y(value interface{}, compatibility... interface{}) *TestTable {
-	if len(compatibility) == len(t.values) {
-		for i, v := range t.values {
-			if column, ok := t.compatibility[v]; ok {
-				column[value] = compatibility[i]
-			} else {
-				panic(t)
-			}
-		}
-		return t
-	}
-	panic(t)
+	t.YValues = append(t.YValues, value)
+	t.compatibility = append(t.compatibility, append(ValueSet{}, compatibility...))
+	return t
 }
+
 func (t *TestTable) Assess(T *Test) *TestTable {
-	for column, tests := range t.compatibility {
-		for row, expectation := range tests {
-			if result := t.Apply(row, column); result != expectation {
+	for row, x := range t.YValues {
+		for column, y := range t.XValues {
+			expectation := t.compatibility[row][column]
+			if result := t.Apply(x, y); result != expectation {
 				T.Error(fmt.Sprintf("[%v, %v]", row, column), fmt.Sprintf("-> expected %v got %v", expectation, result))
 			}
 		}
 	}
 	return t
 }
+
 func (t *TestTable) Apply(x, y interface{}) (i interface{}) {
 	defer func() { if recover() != nil { i = nil } }()
 	return t.test(x, y)
